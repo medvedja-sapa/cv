84e1dbf187d3c59637c5a4ff96728d489ed968b9
nejcr <ravnik.nejc@gmail.com>
2019-10-07 23:12:04 +0200

    Adds sandbox to puppeter

--- transform.js
+++ transform.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
     await page.goto('https://www.ravnik.org/cv/', {waitUntil: 'networkidle2'});
     await page.pdf({path: 'cv.pdf', format: 'A4'});
