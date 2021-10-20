cfcf2f170c7feb55e5dca5fa680187fd9f1df328
nejcr <ravnik.nejc@gmail.com>
2021-10-20 22:54:58 +0200

    Fixes puppeter

--- transform.js
+++ transform.js
@@ -3,7 +3,8 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
-    await page.goto('https://www.ravnik.org/cv/', {waitUntil: 'networkidle2'});
+    await page.goto('https://www.ravnik.org/cv/', {waitUntil: ['networkidle2','domcontentloaded','load']});
+
     await page.pdf({path: 'cv.pdf', format: 'A4'});
 
     await browser.close();
