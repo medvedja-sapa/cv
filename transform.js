0d852995a700e35f8c79f407852ed271c0d96e3d
Nejc Ravnik <ravnik.nejc@gmail.com>
2022-01-19 18:45:14 +0100

    update script

--- transform.js
+++ transform.js
@@ -3,9 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
-    await page.goto('https://www.ravnik.org/cv/', {waitUntil: ['networkidle2','domcontentloaded','load']});
-
-    await page.pdf({path: 'cv.pdf', format: 'A4'});
-
+    await page.goto('https://www.ravnik.org/cv/', {waitUntil: ['networkidle2','domcontentloaded','load'],timeout: 20000});
+    await page.pdf({path: 'cv.pdf', format: 'A4',timeout:20000});
     await browser.close();
 })();
